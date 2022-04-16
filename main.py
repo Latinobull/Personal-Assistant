ec95c1e45ebe451f1a78be0fffeab7a356175113
Donnahue George <Donnahuegjr@gmail.com>
2022-04-16 14:43:20 -0400

    created google map search capabilities

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from time import ctime
 import time
 from gtts import gTTS
 from playsound import playsound
+import webbrowser
 
 
 def listen():
@@ -38,18 +39,23 @@ def digital_assistant(data):
     if 'what time is it' in data:
         listening = True
         response(ctime())
-    if 'stop listening' in data:
+    if "where is" in data:
+        listening = True
+        data = data.split(" ")
+        location_url = "https://www.google.com/maps/place/" + str(data[2])
+        response("Hold on DJ, I will show you where " + data[2] + " is.")
+        webbrowser.get().open_new_tab(location_url)
+        listening = False
+        response('Have a great day')
+    if data == '':
         listening = False
         response('Have a great day')
         return listening
-    else:
-        listening = False
-        response('Have a great day')
     return listening
 
 
-time.sleep(1)
-response('How can I help You Donnahue?')
+time.sleep(.5)
+response('How can I help You DJ?')
 listening = True
 while listening == True:
     data = listen()
