322b9b8ad48e7522ce3b38f0e0c8060ba4a04a34
Donnahue George <Donnahuegjr@gmail.com>
2022-04-29 10:43:56 -0400

    adding new feature to get weather information of city user speaks about

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from operator import truediv
 import os
 import speech_recognition as sr
 import datetime
@@ -5,6 +6,9 @@ import time
 from gtts import gTTS
 from playsound import playsound
 import webbrowser
+from dotenv import load_dotenv
+import requests
+load_dotenv()
 
 
 def listen():
@@ -49,12 +53,33 @@ def digital_assistant(data):
         listening = False
         response('Have a great day')
     if 'show me' in data:
+        listening = True
         data = data.split(' ')
         data = ' '.join(data[2:])
         location_url = f'https://www.google.com/search?q={data}'
         response(f'Here is {data} on Google')
         webbrowser.get().open_new_tab(location_url)
         print(data)
+    if 'what is the weather in' in data:
+        listening = True
+        APIKEY = os.getenv('API-KEY')
+        weather_url = "http://api.openweathermap.org/data/2.5/weather?"
+        data = data.split(" ")
+        location = str(data[5])
+        url = f'{weather_url}appid={APIKEY}&q={location} '
+        js = requests.get(url).json()
+        if js["cod"] != "404":
+            weather = js["main"]
+            temp = round((weather["temp"] - 273.15) * 9/5 + 32)
+            hum = weather["humidity"]
+            desc = js["weather"][0]["description"]
+            resp_string = " The temperature is " + \
+                str(temp) + " The humidity is " + str(hum) + \
+                " and The weather description is " + str(desc)
+            response(resp_string)
+        else:
+            response("City Not Found")
+
     if data == '':
         listening = False
         response('Have a great day')
